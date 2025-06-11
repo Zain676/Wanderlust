e3ce66e7f6e74d5a0545df33a21c736dc4957f32
Zain676 <mzainq9@gmail.com>
2025-06-11 14:24:44 +0500

    initial commit

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,27 +1,21 @@
 const cloudinary = require("cloudinary").v1;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-// Configure with secure settings
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-  secure: true // Force HTTPS
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: async (req, file) => {
-    return {
-      folder: "wanderlust",
-      allowed_formats: ["png", "jpg", "jpeg"],
-      transformation: [{ width: 800, crop: "scale" }],
-      public_id: `${Date.now()}-${file.originalname.split('.')[0]}`
-    };
-  }
+  params: {
+    folder: "wanderlust",
+    allowedFormats: ["png", "jpg", "jpeg"],
+  },
 });
 
 module.exports = {
-  cloudinary,
-  storage
-};
+    cloudinary,
+    storage,
+}
